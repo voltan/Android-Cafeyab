720c72ee020d934750e794ee85c4c5426ce9af76
voltan <djvoltan@gmail.com>
2017-03-27 13:15:35 +0430

    Work on app

--- app/src/main/java/com/faragostaresh/cafeyab/MainActivity.java
+++ app/src/main/java/com/faragostaresh/cafeyab/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.MenuItem;
 import android.view.View;
