b05c483574266f565ffa9fef20d9a97f5812a9c3
voltan <djvoltan@gmail.com>
2017-04-02 12:56:58 +0430

    Work on app

--- app/src/main/java/com/faragostaresh/cafeyab/MainActivity.java
+++ app/src/main/java/com/faragostaresh/cafeyab/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -61,11 +63,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // Set for support RTL
         getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
 
         // Set hide actionBar
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
         // Set bottom navigation
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -142,5 +148,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_home:
+                Intent intentHome = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intentHome);
+                break;
+
+            case R.id.menu_user:
+                Intent intentUser = new Intent(getApplicationContext(), UserActivity.class);
+                startActivity(intentUser);
+                break;
+
+            case R.id.menu_about:
+                Intent intentAbout = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intentAbout);
+                break;
+
+            case R.id.menu_search:
+                Intent intentSearch = new Intent(getApplicationContext(), CafeListActivity.class);
+                startActivity(intentSearch);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
