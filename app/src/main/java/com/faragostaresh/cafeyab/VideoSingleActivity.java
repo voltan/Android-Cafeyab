16a47035bde1f7bbf8bd055b4b4a25f7d6fdb140
voltan <djvoltan@gmail.com>
2017-04-08 16:52:28 +0430

    Work on app

--- app/src/main/java/com/faragostaresh/cafeyab/VideoSingleActivity.java
+++ app/src/main/java/com/faragostaresh/cafeyab/VideoSingleActivity.java
@@ -2,12 +2,16 @@ package com.faragostaresh.cafeyab;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -16,10 +20,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.faragostaresh.adaptor.VideoListAdapter;
+import com.faragostaresh.model.ItemList;
+import com.orangegangsters.github.swipyrefreshlayout.library.SwipyRefreshLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VideoSingleActivity extends AppCompatActivity {
 
@@ -27,6 +37,10 @@ public class VideoSingleActivity extends AppCompatActivity {
 
     public static String videoUrl = "";
     public static String itemId = "";
+
+    private List<ItemList> myVideoList = new ArrayList<ItemList>();
+    private ListView listView;
+    private VideoListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,16 @@ public class VideoSingleActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         itemId = extras.getString("itemId");
         videoUrl = "https://www.cafeyab.com/video/json/videoSingle/id/" + itemId;
+
+
+
+        // Set for list of items
+        adapter = new VideoListAdapter(this, myVideoList);
+        listView = (ListView) findViewById(R.id.videoRelatedlist);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new VideoSingleActivity.ListVewiClickListener());
+        //myVideoList.clear();
+
 
         Log.d(TAG, "Single item url : " + videoUrl);
 
@@ -70,6 +94,34 @@ public class VideoSingleActivity extends AppCompatActivity {
                             playerWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
                             playerWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);;
                             playerWebView.loadUrl(json.getString("qmeryDirect"));
+
+
+                            Log.d(TAG, "videoRelated array: " + json.getJSONArray("videoRelated"));
+
+                            JSONArray jsonArrayRelated = json.getJSONArray("videoRelated");
+                            if (jsonArrayRelated.length() > 0) {
+                                for (int i = 0; i < jsonArrayRelated.length(); i++) {
+                                    try {
+                                        JSONObject obj = jsonArrayRelated.getJSONObject(i);
+
+                                        Log.d(TAG, "videoRelated single: " + jsonArrayRelated.getJSONObject(i));
+                                        Log.d(TAG, "videoRelated object: " + obj);
+                                        Log.d(TAG, "videoRelated title: " + obj.getString("title"));
+                                        Log.d(TAG, "videoRelated id: " + obj.getString("id"));
+                                        Log.d(TAG, "videoRelated mediumUrl: " + obj.getString("mediumUrl"));
+
+                                        ItemList video = new ItemList();
+                                        video.setTitle(obj.getString("title"));
+                                        video.setItemID(obj.getString("id"));
+                                        video.setThumbnailUrl(obj.getString("mediumUrl"));
+                                        myVideoList.add(video);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+
+
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -121,6 +173,26 @@ public class VideoSingleActivity extends AppCompatActivity {
                 break; */
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private class ListVewiClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            ItemList getselected = (ItemList) (listView.getItemAtPosition(position));
+            itemId = getselected.getItemId();
+            displayView(itemId);
+        }
+
+        private void displayView(String itemId) {
+            // Start New activity with a request to Show Selected News
+            Intent intent = new Intent(getApplicationContext(), VideoSingleActivity.class);
+            intent.putExtra("itemId", itemId);
+            startActivity(intent);
+
+        }
     }
 
     @Override
