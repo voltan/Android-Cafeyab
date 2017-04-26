1918069c93553c5dfb4c2040f6f1895dd3ab32ca
voltan <djvoltan@gmail.com>
2017-04-26 21:41:07 +0430

    Work on app

--- app/src/main/java/com/faragostaresh/cafeyab/MapSingleActivity.java
+++ app/src/main/java/com/faragostaresh/cafeyab/MapSingleActivity.java
@@ -186,7 +186,7 @@ public class MapSingleActivity extends AppCompatActivity
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "در حال یافتن مکان شما", Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
