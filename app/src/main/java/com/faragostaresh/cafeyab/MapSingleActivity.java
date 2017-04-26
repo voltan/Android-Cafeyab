a7de8f7a588aae72887acd3105f235c5e4cef076
voltan <djvoltan@gmail.com>
2017-04-26 21:34:27 +0430

    Work on app

--- app/src/main/java/com/faragostaresh/cafeyab/MapSingleActivity.java
+++ app/src/main/java/com/faragostaresh/cafeyab/MapSingleActivity.java
@@ -1,13 +1,63 @@
 package com.faragostaresh.cafeyab;
 
+import android.*;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.content.IntentSender;
+import android.location.Address;
+import android.location.Geocoder;
+import android.widget.Toast;
 
-public class MapSingleActivity extends AppCompatActivity {
+import com.faragostaresh.adaptor.PermissionUtils;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapSingleActivity extends AppCompatActivity
+        implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private boolean mPermissionDenied = false;
+
+    private GoogleMap mMap;
 
     public static String itemId = "";
     public static String itemTitle = "";
@@ -34,6 +84,8 @@ public class MapSingleActivity extends AppCompatActivity {
 
         setTitle(itemTitle + " بر روی نقشه");
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -68,5 +120,108 @@ public class MapSingleActivity extends AppCompatActivity {
                 break; */
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /* @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng cafe = new LatLng(Double.parseDouble(mapLatitude), Double.parseDouble(mapLongitude));
+        mMap.addMarker(new MarkerOptions().position(cafe).title(itemTitle));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cafe, Integer.parseInt(mapZoom)));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMarkerClickListener(this);
+
+    } */
+
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a marker in Sydney and move the camera
+        LatLng cafe = new LatLng(Double.parseDouble(mapLatitude), Double.parseDouble(mapLongitude));
+
+        // Setup map
+        mMap = map;
+
+        // Setup location
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+
+        // Setup map ui
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setIndoorLevelPickerEnabled(true);
+        mMap.setTrafficEnabled(true);
+
+        // Setup map marker
+        mMap.addMarker(new MarkerOptions().position(cafe).title(itemTitle));
+
+        // Setup map camera zoom
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cafe, Integer.parseInt(mapZoom)));
+
+        // Setup map type
+        if (mapType.equalsIgnoreCase("SATELLITE")) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else if (mapType.equalsIgnoreCase("HYBRID")) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        } else if (mapType.equalsIgnoreCase("TERRAIN")) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        } else if (mapType.equalsIgnoreCase("ROADMAP")) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
