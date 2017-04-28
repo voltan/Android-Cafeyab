998141818da0ea16d3486054947de0ddc26a30e8
voltan <djvoltan@gmail.com>
2017-04-28 13:00:33 +0430

    Work on app

--- app/src/main/java/com/faragostaresh/cafeyab/EventSingleActivity.java
+++ app/src/main/java/com/faragostaresh/cafeyab/EventSingleActivity.java
@@ -1,20 +1,76 @@
 package com.faragostaresh.cafeyab;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+import com.faragostaresh.adaptor.MyTagHandler;
+import com.faragostaresh.adaptor.VideoListAdapter;
+import com.faragostaresh.app.CafeyabApplication;
+import com.faragostaresh.model.ItemList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class EventSingleActivity extends AppCompatActivity {
 
-    public static String itemId;
+    private static final String TAG = EventSingleActivity.class.getSimpleName();
+
+    ImageLoader imageLoader = CafeyabApplication.getInstance().getImageLoader();
+
+    Context context = this;
+
+    public static String eventUrl = "";
+    public static String itemId = "";
+    public static String itemTitle = "";
+    public static String itemUrl = "https://www.cafeyab.com";
+    public static String largeUrl = "";
+
+    public TextView txtSummary;
+    public TextView txtDescription;
+    public TextView registerDetails;
+
+    private ListView mainListView;
+    private ArrayAdapter<String> listAdapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_single);
+
+        // Set toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Set for support RTL
         getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
@@ -22,8 +78,211 @@ public class EventSingleActivity extends AppCompatActivity {
         // Get search information
         Bundle extras = getIntent().getExtras();
         itemId = extras.getString("itemId");
+        itemTitle = extras.getString("itemTitle");
+        eventUrl = "https://www.cafeyab.com/event/json/eventSingle/id/" + itemId;
 
-        setTitle(itemId);
+        Log.d(TAG, "Single item url : " + eventUrl);
+
+        // Set title
+        setTitle(itemTitle);
+
+        NetworkImageView thumbnail = (NetworkImageView) findViewById(R.id.photo);
+
+        //Creating a json array request
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(eventUrl,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //VideoList videoSingle = new VideoList();
+                        Log.d(TAG, response.toString());
+                        JSONObject json = null;
+                        try {
+                            json = response.getJSONObject(0);
+
+                            // Set title
+                            setTitle(json.getString("title"));
+
+                            // Set info for layout
+                            thumbnail.setImageUrl(json.getString("largeUrl"), imageLoader);
+
+                            // Set info for layout
+                            //TextView textViewTitle = (TextView) findViewById(R.id.viewTitle);
+                            //textViewTitle.setText(json.getString("title"));
+
+                            //TextView textSubViewTitle = (TextView) findViewById(R.id.viewSubTitle);
+                            //textViewTitle.setText(json.getString("subtitle"));
+
+                            txtSummary = (TextView) findViewById(R.id.summary);
+                            if (!json.getString("text_summary").isEmpty()) {
+                                txtSummary.setText(json.getString("text_summary"));
+                            } else {
+                                txtSummary.setVisibility(View.GONE);
+                            }
+
+                            txtDescription = (TextView) findViewById(R.id.description);
+                            if (!json.getString("text_description").isEmpty()) {
+                                txtDescription.setText(Html.fromHtml(json.getString("text_description"), null, new MyTagHandler()));
+                            } else {
+                                txtDescription.setVisibility(View.GONE);
+                            }
+
+                            registerDetails = (TextView) findViewById(R.id.register_details);
+                            if (!json.getString("register_details").isEmpty()) {
+                                registerDetails.setText(Html.fromHtml(json.getString("text_description"), null, new MyTagHandler()));
+                            } else {
+                                registerDetails.setVisibility(View.GONE);
+                                TextView txtheaderRegister = (TextView) findViewById(R.id.register_details_title);
+                                txtheaderRegister.setVisibility(View.GONE);
+                            }
+
+                            // Set item url
+                            itemUrl = json.getString("eventUrl");
+
+                            // Set item largeUrl
+                            largeUrl = json.getString("largeUrl");
+
+
+                            String[] attributes1 = new String[]{};
+                            ArrayList<String> attributesList1 = new ArrayList<String>();
+                            attributesList1.addAll(Arrays.asList(attributes1));
+                            listAdapter1 = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_row_cafe_attributes, attributesList1);
+                            if (!json.getString("time_view").isEmpty()) {
+                                listAdapter1.add("زمان برگزاری : " + json.getString("time_view"));
+                            }
+                            if (!json.getString("organizer_name").isEmpty()) {
+                                listAdapter1.add("سازمان دهنده : " + json.getString("organizer_name"));
+                            }
+                            if (!json.getString("register_price_view").isEmpty()) {
+                                listAdapter1.add("هزینه ثبت نام  : " + json.getString("register_price_view"));
+                            }
+                            if (!json.getString("address").isEmpty()) {
+                                listAdapter1.add("آدرس : " + json.getString("address"));
+                                listAdapter1.add(" ");
+                            }
+                            mainListView = (ListView) findViewById(R.id.listView);
+                            mainListView.setAdapter(listAdapter1);
+                            Integer attributeCount1 = listAdapter1.getCount();
+                            if (attributeCount1 <= 0) {
+                                TextView txtheader1 = (TextView) findViewById(R.id.listEventTitle);
+                                txtheader1.setVisibility(View.GONE);
+                            } else {
+                                setListViewHeightBasedOnChildren(mainListView);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                    }
+                });
+        //Creating request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Adding request to the queue
+        requestQueue.add(jsonArrayRequest);
+
+        // Set shear bottom
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.share_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Intent> targetShareIntents = new ArrayList<Intent>();
+
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("*/*");
+
+                List<ResolveInfo> resInfos = context.getPackageManager().queryIntentActivities(shareIntent, 0);
+                for (ResolveInfo ri : resInfos) {
+
+                    String packageName = ri.activityInfo.packageName;
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_SEND);
+                    intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+                    intent.setPackage(packageName);
+
+                    if (packageName.contains("com.twitter.android")) {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("com.facebook.katana")) {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("com.instagram")) {
+
+                        intent.setType("image/jpeg");
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("com.pinterest")) {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("com.google.android.gm")) {
+
+                        intent.setType("image/*");
+                        intent.putExtra(Intent.EXTRA_TITLE, itemTitle);
+                        intent.putExtra(Intent.EXTRA_SUBJECT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("com.hootsuite.droid.full")) {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("com.facebook.pages.app")) {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        targetShareIntents.add(intent);
+
+                    } else if (packageName.contains("org.telegram.messenger")) {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        targetShareIntents.add(intent);
+
+                    } /* else {
+
+                        intent.setType("text/plain");
+                        intent.putExtra(Intent.EXTRA_TEXT, itemTitle);
+                        intent.putExtra(Intent.EXTRA_TEXT, itemUrl);
+                        intent.putExtra(Intent.EXTRA_STREAM, largeUrl);
+                        targetShareIntents.add(intent);
+
+                    } */
+                }
+
+                Intent chooserIntent = Intent.createChooser(targetShareIntents.remove(0), "Choose app to share");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toArray(new Parcelable[]{}));
+                startActivity(chooserIntent);
+
+            }
+        });
     }
 
     @Override
@@ -58,5 +317,25 @@ public class EventSingleActivity extends AppCompatActivity {
                 break; */
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
